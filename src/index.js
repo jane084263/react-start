12faa78831060119c3e207f058f4f1a5e9941a86
孙霖霖 <sunlinlin084263@163.com>
2018-09-21 14:04:04 +0800

    create react APP

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+//
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import React,{Component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>hello react</h1>
+            </div>
+        )
+    }
+}
+ReactDOM.render(<App/>,document.getElementById('root'));
